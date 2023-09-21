11426650f1dca31feda3ea4b5f6a08ba0f4fe198
Xiaohan Mu <mxh126qq@126.com>
2023-09-21 14:23:11 +0100

    change content of veryHappy

--- veryHappy.java
+++ veryHappy.java
@@ -1,9 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		//example here:
-		//example code:
-		System.out.println("Hello world");
-		System.out.println("Hello world");
+		System.out.println("I'm very happy:)");
 	}
 }
 	
