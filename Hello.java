28d94e7f80fc05b72fc70e7c4061519755b2d4eb
Xiaohan Mu <mxh126qq@126.com>
2023-09-21 14:12:41 +0100

    Rename noHello.java to Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String[] args){
+		//example here:
+		//example code:
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+}
+	
